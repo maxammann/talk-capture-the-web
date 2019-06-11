ead231c04498311f6a5112a537ad039513091e0e
Maximilian Ammann <max@maxammann.org>
2019-06-11 13:39:01 +0200

    Update watch script

--- watch.sh
+++ watch.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-while true; do inotifywait -r -e modify --exclude=".html" . && pandoc --syntax-definition=bash.xml --highlight-style zenburn-custom.theme --css style.css --css Font-Awesome/css/all.css -t revealjs -s -o slides.html slides.md -V revealjs-url=./reveal.js; done
+build () {
+    pandoc --syntax-definition=bash.xml --highlight-style zenburn-custom.theme --css style.css --css Font-Awesome/css/all.css -t revealjs -s -o deploy/index.html slides.md -V revealjs-url=./reveal.js --self-contained
+}
+
+build
+
+while true; do inotifywait -r -e modify --exclude=".html" . && build ; done
